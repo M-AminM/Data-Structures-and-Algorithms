a1a742f151e60ed9f76302bd472a9663154ddd59
Amin <Asgharnezhad.Mohamad@OKCO.ir>
2024-07-27 12:48:51 +0330

    Implement liked list

--- src/index.js
+++ src/index.js
@@ -1,5 +1,7 @@
 // import { Array } from "./dataStructures/array";
 
+import { LinkedList } from "./dataStructures/linkedList";
+
 // const array = new Array();
 // array.push("first item");
 // array.push("second item");
@@ -24,17 +26,27 @@
 // hashTable.set("gholam", 22);
 // console.log(hashTable.keys());
 
-const testArr = [2, 5, 1, 2, 3, 5, 1, 2, 4];
-function ads(input) {
-  let map = {};
-  for (let i = 0; i < input.length; i++) {
-    if (map[input[i]]) {
-      return input[i];
-    } else {
-      map[input[i]] = 1;
-    }
-    console.log(map);
-  }
-}
+// const testArr = [2, 5, 1, 2, 3, 5, 1, 2, 4];
+// function ads(input) {
+//   let map = {};
+//   for (let i = 0; i < input.length; i++) {
+//     if (map[input[i]]) {
+//       return input[i];
+//     } else {
+//       map[input[i]] = 1;
+//     }
+//     console.log(map);
+//   }
+// }
 
-console.log(ads(testArr));
+// console.log(ads(testArr));
+
+const linkedList = new LinkedList(10);
+linkedList.append(5);
+linkedList.append(18);
+linkedList.append(21);
+linkedList.prepend(-222);
+linkedList.printList();
+linkedList.insert(2, 1234);
+linkedList.insert(4, 54231);
+linkedList.remove(2);
